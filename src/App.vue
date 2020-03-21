553d28ed5455df11e326deb1047c9e3e4c2c1956
PauloAlves8039 <pj.alves547@gmail.com>
2020-03-21 20:17:40 -0300

    Commit inicial do repositório

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Calculadora msg="Calculadora Básica"/>
+  </div>
+</template>
+
+<script>
+import Calculadora from './components/Calculadora.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calculadora
+  }
+}
+</script>
+
+<style>
+@font-face {
+    font-family: FonteTitle;
+    src: url("../src/assets/fonts/Saira-Bold.ttf");
+}
+#app {
+  font-family: FonteTitle;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Calculadora.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+@font-face {
+    font-family: FonteBody;
+    src: url("../assets/fonts/Armata-Regular.ttf");
+  }
+  
+</style>
